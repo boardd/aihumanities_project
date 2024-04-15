ec207d0e862520a98904cf81a9a5ba2357508193
jaimiele <jaimiele@andrew.cmu.edu>
2024-04-15 16:43:47 -0400

    backend for image upload

--- server/backend.py
+++ server/backend.py
@@ -1,9 +1,34 @@
 # backend.py
-from flask import Flask, jsonify
+from flask import Flask, jsonify, flash, request, redirect, url_for, session
+from werkzeug.utils import secure_filename
 from flask_cors import CORS
+import os
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger('HELLO WORLD')
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
 
 app = Flask(__name__)
 CORS(app)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route("/qrimage_upload", methods=['POST'])
+def QRImage_upload():
+    target=os.path.join(UPLOAD_FOLDER,'test_docs')
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    logger.info("welcome to upload`")
+    file = request.files['file'] 
+    filename = secure_filename(file.filename)
+    destination="/".join([target, filename])
+    file.save(destination)
+    session['uploadFilePath']=destination
+    response="File successfully uploaded!"
+    return response
 
 @app.route("/")
 def home():

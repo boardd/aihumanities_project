d154aeef5977b0bcd2031b2dca3eb60e00e18849
jaimiele <jaimiele@andrew.cmu.edu>
2024-04-17 19:09:11 -0400

    completed home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,6 +11,9 @@ const Home = () => {
                 <h2>Welcome to MyGalleryGuide: Your Personalized Souvenir Creator at the Carnegie Museum of Art</h2>
                 <p>MyGalleryGuide was created as a final project for the course AI for Humanities. We decided that AI could be utilized to in such a museum-heavy place like Pittsburgh to help personalize your trip there and give you something unique to you when you leave.</p>
                 <h3>How It Works:</h3>
+                <p>1. Start Scanning QR Codes: As you explore the museum's diverse collections, use the menu tab to scan/upload as many QR codes from the paintings you enjoyed, found interesting, or overall would want to be a component of your unique painting.</p>
+                <p>2. With just a click, your personalized artwork is created for you to enjoy and serves as a memento of your trip!</p>
+                <p>We hope you enjoy MyGalleryGuide and leave the Carnegie Museum of Art with something truly personal and memorable.</p>
             </div>
         </div>
       );

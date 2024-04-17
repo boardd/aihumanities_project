ecbf152c469eb2a5333fda362e72e47ab1481c0d
jaimiele <jaimiele@andrew.cmu.edu>
2024-04-17 17:38:18 -0400

    project description

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,7 +8,9 @@ const Home = () => {
             <Header />
             <img src={require("../images/Carnegie-Museum-Logo.jpg")} style={{height:"600px", margin:"100px", float: "left"}} alt="Carnegie Museum of Art."></img>
             <div className='Content'>
-                <p>Description of project here. (mention carnegie museum, qr codes, instructions on how to use application)</p>
+                <h2>Welcome to MyGalleryGuide: Your Personalized Souvenir Creator at the Carnegie Museum of Art</h2>
+                <p>MyGalleryGuide was created as a final project for the course AI for Humanities. We decided that AI could be utilized to in such a museum-heavy place like Pittsburgh to help personalize your trip there and give you something unique to you when you leave.</p>
+                <h3>How It Works:</h3>
             </div>
         </div>
       );
